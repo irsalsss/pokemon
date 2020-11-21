31577e028b30ce938595fc5cee91d8051be6cc30
Muhamad Irsal Firanyah <muhamadirsal@gmail.com>
2020-11-21 11:44:54 +0700

    add underscore js

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { usePokemon } from '../context/PokemonContext';
 import { useParams } from "react-router-dom";
+import './DetailPage.scss';
 
 const DetailPage = () => {
   const { id } = useParams()
@@ -13,8 +14,21 @@ const DetailPage = () => {
     }
   }, [])
   return (
-    <div>
-      ada
+    <div className='container-detail-page'>
+      {!_.isEmpty(singlePokemonData) && (
+        <React.Fragment>
+          <h2 className='title-page'>{singlePokemonData.name}</h2>
+
+          <div className='image-wrapper'>
+            <img alt={singlePokemonData.name} width='96px' height='96px' src={singlePokemonData?.sprites?.front_default} />
+            <img alt={singlePokemonData.name} width='96px' height='96px' src={singlePokemonData?.sprites?.back_default} />
+          </div>
+
+          <div className='pokemon-information'>
+
+          </div>
+        </React.Fragment>
+      )}
     </div>
   )
 }
