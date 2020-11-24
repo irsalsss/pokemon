6be54b2b4a840ec8b990c38d10e79930755ff95f
Muhamad Irsal Firanyah <muhamadirsal@gmail.com>
2020-11-24 12:38:46 +0700

    add initial unit testing

---
+++ src/utils/Helper.test.js
@@ -0,0 +1,30 @@
+import { urlToId, successfullLogic } from "./Helper";
+
+const mockMath = Object.create(global.Math);
+mockMath.random = () => 0.5;
+global.Math = mockMath;
+
+describe("Test all helper function", () => {
+  afterEach(() => {
+    jest.spyOn(global.Math, 'random').mockRestore();
+  })
+
+  test("function urlToId", () => {
+    expect(urlToId("https://pokeapi.co/api/v2/pokemon/1/")).toBe("1")
+    expect(urlToId("https://pokeapi.co/api/v2/pokemon/11/")).toBe("11")
+    expect(urlToId("https://pokeapi.co/api/v2/pokemon/111/")).toBe("111")
+    expect(urlToId("https://pokeapi.co/api/v2/pokemon/1111/")).toBe("1111")
+  })
+
+
+  test("function catch pokemon should success", () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(60);
+    expect(successfullLogic()).toBe(true)
+  })
+
+  test("function catch pokemon should failed", () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(49);
+    expect(successfullLogic()).toBe(true)
+  })
+
+})
