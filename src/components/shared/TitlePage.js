0413ce95f2e70db8a37a7c8a361c8d9ab856d025
Muhamad Irsal Firanyah <muhamadirsal@gmail.com>
2020-11-25 15:48:16 +0700

    remove 0

--- src/components/shared/TitlePage.js
+++ src/components/shared/TitlePage.js
@@ -6,9 +6,9 @@ const TitlePage = ({ title, total }) => {
   return (
     <React.Fragment>
       <h2 data-cy='title-page' data-testid='title-page' className='title-page'>{title || ""}</h2>
-      { total &&
+      { total != undefined &&
         <h2 data-testid='total-my-pokemon' data-cy='total-my-pokemon' className='title-page'>
-            (Total Owned: {total || 0})
+            (Total Owned: {total})
         </h2>
       }
     </React.Fragment>
